a7c66814470da677d80de82b3777505516635f8e
Timéo BAUDAT <timeo.baudat@agrocampus-ouest.fr>
2024-09-29 17:18:22 +0200

    correction graphique

--- ProjetVelo/server.R
+++ ProjetVelo/server.R
@@ -14,12 +14,15 @@ library(lubridate)
 library(factoextra)
 #library(htmlwidgets)
 library(plotly)
+library(tidyverse)
+
+
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-
   
-#TIM
+  
+  #TIM
   
   #Création d'une variable avec la date et heure dans le bon format
   df <- read.table('SeoulBikeData.csv', sep = ",", dec=".", header=TRUE, stringsAsFactors = TRUE, fileEncoding = "ISO-8859-1")
@@ -27,7 +30,22 @@ function(input, output, session) {
   df$Hour <- paste0(sprintf("%02d", df$Hour), ":00")
   df$DateHourtemp <- paste(df$Date, df$Hour)
   df$DateHour <- ymd_hm(df$DateHourtemp)
-  print(df)
+  df$Jour <- day(df$Date)
+  df$Mois <- month(df$Date)
+  df$Année <- year(df$Date)
+  str(df)
+  
+  colnames(df) <- c("Date", "Rented.Bike.Count", "Heure", "Température",
+                    "Humidité", "Vitesse du vent", "Visibilité", 
+                    "Température du point de rosée",
+                    "Rayonnement solaire", "Précipitations", "Chutes de neige", "Saisons",
+                    "Vacances", "Jour de fonctionnement", "DateHourTemp", "DateHour", "Jour",
+                    "Mois","Annee")
+  
+  quant_vars <- c("Température", "Humidité", "Vitesse du vent", "Visibilité",
+                  "Température du point de rosée", "Rayonnement solaire", 
+                  "Précipitations", "Chutes de neige")
+  qual_vars <- c("Heure", "Jour", "Mois", "Saisons", "Vacances", "Jour de fonctionnement")
   
   output$linePlot <- renderPlotly({
     req(input$dates)  
@@ -41,114 +59,100 @@ function(input, output, session) {
     # Créer l'objet xts avec les données filtrées
     dy_data <- xts(filtered_data$Rented.Bike.Count, order.by = filtered_data$DateHour)
     
-    if (input$graphSelect == "Graphique 1") {
-      # Créer le graphique plotly
-      plot_ly(filtered_data, x = ~DateHour, y = ~Rented.Bike.Count, type = 'scatter', mode = 'lines+markers',
+    if (input$varSelect %in% quant_vars) {
+      
+      plot_ly(filtered_data, 
+              x = ~DateHour, 
+              y = ~Rented.Bike.Count, 
+              type = 'scatter', 
+              mode = 'lines+markers',
               text = ~paste("Date:", DateHour, "<br>Valeur:", Rented.Bike.Count),
+              name = "Nombre de vélos loués",
               hoverinfo = 'text') %>%
-        layout(title = "Quantité de vélos loués dans la ville de Séoul")
+        # Ajouter la deuxième variable sélectionnée par l'utilisateur
+        add_trace(y = ~filtered_data[[input$varSelect]],  
+                  type = 'scatter', 
+                  mode = 'lines+markers', 
+                  text = ~paste("Date:", DateHour, "<br>Valeur:", filtered_data[[input$varSelect]]),
+                  hoverinfo = 'text', 
+                  name = input$varSelect, 
+                  yaxis = "y2")  %>% 
+        layout(title = "Quantité de vélos loués dans la ville de Séoul",
+               yaxis = list(title = "Vélos loués"),
+               yaxis2 = list(title = input$varSelect, overlaying = "y", side = "right"),
+               xaxis = list(title = "Date et Heure"),
+               hovermode = "closest")  # Définir le mode de survol
+               
+    } else if (input$varSelect %in% qual_vars) {
+      print("blabla")
       
-    } else if (input$graphSelect == "Graphique 2") {
-     
-    } else if (input$graphSelect == "Graphique 3") {
+    } 
+    
+  })
       
-    }
-    
-    
-  })
-  
-#ELISE
-  
-  # Chargement des données - Si dta_MFA est déjà chargé
-  dta_MFA <- reactive({
-    dta_MFA <- dta[,-1]  # Suppression de la première colonne
-    return(dta_MFA)
-  })
-  
-  # Fonction qui effectue l'AFM
-  res_AFM <- eventReactive(input$run_afm, {
-    # Lancement de l'AFM lorsque l'utilisateur clique sur le bouton
-    dta_data <- dta_MFA()
-    res <- MFA(dta_data, 
-               group = c(8, 7),          # Nombre de variables dans chaque groupe
-               type = c("s", "n"),        # Type des variables : "s" pour quanti, "n" pour quali
-               name.group = c("Météo", "Temporalité"),
-               graph = FALSE)             # Désactivation des graphiques automatiques
-    return(res)
-  })
-  
-  # Résumé des résultats de l'AFM
-  output$afm_summary <- renderPrint({
-    req(res_AFM())  # Attendre que l'AFM soit lancée
-    summary(res_AFM())
-  })
-  
-  # Graphique de l'AFM
-  output$afm_plot <- renderPlot({
-    req(res_AFM())
-    fviz_mfa_var(res_AFM(), "group")  # Visualisation des groupes
-  })
-  
-#MARINE
-  
-  # Calcul du nombre de lignes
-  output$nb_lignes <- renderText({
-    nrow(df)
-  })
-  
-  output$var_reponse <- renderUI({
-    # Nom de la variable réponse et sa description
-    reponse_var <- list(
-      "Rented.Bike.Count" = "Nombre de vélos loués (Variable quantitative sans unité)" 
-    )
-    
-    # Formatage pour affichage de la variable réponse (sans gras)
-    HTML(paste(sapply(names(reponse_var), function(var) {
-      paste("<strong>", var, ":</strong> ", reponse_var[[var]])
-    }), collapse = "<br>"))
-  })
-  # Variables explicatives quantitatives avec descriptions et unités directement incluses
-  output$var_quant <- renderUI({
-    # Liste des variables quantitatives avec leurs unités et descriptions
-    quant_vars <- list(
-      "Temperature..C." = "Température en °C",
-      "Humidity..." = "Humidité en %",
-      "Wind.speed..m.s." = "Vitesse du vent en m/s",
-      "Visibility..10m." = "Visibilité en 10m",
-      "Dew.point.temperature..C." = "Température du point de rosée en °C",
-      "Solar.Radiation..MJ.m2." = "Rayonnement solaire en MJ/m2",
-      "Rainfall.mm." = "Précipitation en mm",
-      "Snowfall..cm." = "Chutes de neige en cm"
-    )
-    
-    HTML(paste(sapply(names(quant_vars), function(var) {
-      paste("<strong>", var, ":</strong> ", quant_vars[[var]])
-    }), collapse = "<br>"))
-  })
-
-  # Variables explicatives qualitatives avec descriptions
-  output$var_qual <- renderUI({
-    # Liste des variables qualitatives avec descriptions
-    qual_vars <- list(
-      "Date" = "Date (jour, mois et année",
-      "Hour" = "Heure de la journée",
-      "Seasons" = "Saison : hiver, printemps, été ou automne",
-      "Holiday" = "Vacances : oui ou non",
-      "Functioning.Day" = "Jour de fonctionnement : oui ou non"
-    )
-    
-    # Formatage des variables qualitatives
-    HTML(paste(sapply(names(qual_vars), function(var) {
-      paste("<strong>", var, ":</strong> ", qual_vars[[var]])
-    }), collapse = "<br>"))
-  })
-  
-  #Visualisation du jeu de données 
-  output$data_table <- DT::renderDataTable({
-    DT::datatable(
-      df[,-(15:16)],  # Votre jeu de données
-      options = list(scrollX = TRUE)  # Activer le défilement horizontal
-    )
-  })
+      #ELISE
+      
+      
+      #MARINE
+      
+      # Calcul du nombre de lignes
+      output$nb_lignes <- renderText({
+        nrow(df)
+      })
+      
+      output$var_reponse <- renderUI({
+        # Nom de la variable réponse et sa description
+        reponse_var <- list(
+          "Rented.Bike.Count" = "Nombre de vélos loués (Variable quantitative sans unité)" 
+        )
+        
+        # Formatage pour affichage de la variable réponse (sans gras)
+        HTML(paste(sapply(names(reponse_var), function(var) {
+          paste("<strong>", var, ":</strong> ", reponse_var[[var]])
+        }), collapse = "<br>"))
+      })
+      # Variables explicatives quantitatives avec descriptions et unités directement incluses
+      output$var_quant <- renderUI({
+        # Liste des variables quantitatives avec leurs unités et descriptions
+        quant_vars <- list(
+          "Temperature..C." = "Température en °C",
+          "Humidity..." = "Humidité en %",
+          "Wind.speed..m.s." = "Vitesse du vent en m/s",
+          "Visibility..10m." = "Visibilité en 10m",
+          "Dew.point.temperature..C." = "Température du point de rosée en °C",
+          "Solar.Radiation..MJ.m2." = "Rayonnement solaire en MJ/m2",
+          "Rainfall.mm." = "Précipitation en mm",
+          "Snowfall..cm." = "Chutes de neige en cm"
+        )
+        
+        HTML(paste(sapply(names(quant_vars), function(var) {
+          paste("<strong>", var, ":</strong> ", quant_vars[[var]])
+        }), collapse = "<br>"))
+      })
+      
+      # Variables explicatives qualitatives avec descriptions
+      output$var_qual <- renderUI({
+        # Liste des variables qualitatives avec descriptions
+        qual_vars <- list(
+          "Date" = "Date (jour, mois et année",
+          "Hour" = "Heure de la journée",
+          "Seasons" = "Saison : hiver, printemps, été ou automne",
+          "Holiday" = "Vacances : oui ou non",
+          "Functioning.Day" = "Jour de fonctionnement : oui ou non"
+        )
+        
+        # Formatage des variables qualitatives
+        HTML(paste(sapply(names(qual_vars), function(var) {
+          paste("<strong>", var, ":</strong> ", qual_vars[[var]])
+        }), collapse = "<br>"))
+      })
+      
+      #Visualisation du jeu de données 
+      output$data_table <- DT::renderDataTable({
+        DT::datatable(
+          df[,-(15:16)],  # Votre jeu de données
+          options = list(scrollX = TRUE)  # Activer le défilement horizontal
+        )
+      })
 }
 
